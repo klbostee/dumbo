e3c5cf4fa4d0a46a202ff5d103474eda345f9516
Klaas Bosteels <klbostee@WE02C022.(none)>
2008-07-14 16:42:12 +0200

    bumped version number

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='dumbo',
-      version='0.3',
+      version='0.4',
       py_modules=['dumbo'],
       author='Klaas Bosteels',
       author_email='klaas@last.fm',
