61f249df400bdcd568578f051cb843965054700c
Klaas Bosteels <klbostee@WE02C022.(none)>
2008-07-14 16:41:00 +0200

    changed example a bit

--- examples/itertwice.py
+++ examples/itertwice.py
@@ -1,11 +1,12 @@
 def mapper1(key,value):
     for word in value.split(): yield word,1
 
-def reducer1(key,values):
-    if sum(values) > 1: yield key,
-
 def mapper2(key,value):
-    for letter in value: yield letter,1
+    for letter in value.split()[0]: yield letter,1
+
+def reducer1(key,values):
+    count = sum(values)
+    if count > 1: yield key,count
 
 def reducer2(key,values):
     yield key,sum(values)
@@ -13,6 +14,6 @@ def reducer2(key,values):
 if __name__ == "__main__":
     import dumbo
     job = dumbo.Job()
-    job.additer(mapper1,reducer1)
+    job.additer(mapper1,reducer1,reducer2)
     job.additer(mapper2,reducer2,reducer2)
     job.run()
