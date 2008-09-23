a2d8fbe1a5806670ac9352b7ebf3b2b937eedbf8
Klaas Bosteels <klbostee@WE02C022.(none)>
2008-09-23 18:13:34 +0200

    * added -libpy option * added local streaming * added cat * bumped version number * general cleanup

--- dumbo.py
+++ dumbo.py
@@ -32,7 +32,7 @@ def run(mapper,reducer=None,combiner=None,
         mapconf=None,redconf=None,code_in=False,code_out=False,
         iter=0,newopts={}):
     if len(sys.argv) > 1 and not sys.argv[1][0] == "-":
-        iterarg = 0 # default value
+        iterarg = 0  # default value
         if len(sys.argv) > 2: iterarg = int(sys.argv[2])
         if iterarg == iter:
             if sys.argv[1].startswith("map"):
@@ -60,14 +60,13 @@ def run(mapper,reducer=None,combiner=None,
             if newopts.has_key(key): delindexes.append(index)
         for delindex in reversed(delindexes): del opts[delindex]
         opts += newopts.iteritems()
-        retval = submit(sys.argv[0],opts)
-        if retval != 0: sys.exit(retval)
+        submit(sys.argv[0],opts)
 
 class Job:
     def __init__(self): self.iters = []
     def additer(self,*args,**kwargs): self.iters.append((args,kwargs))
     def run(self):
-        scratch = "tmp/%s-%i" % (sys.argv[0],random.randint(0,sys.maxint))
+        scratch = "/tmp/%s-%i" % (sys.argv[0],random.randint(0,sys.maxint))
         for index,(args,kwargs) in enumerate(self.iters):
             newopts = {}
             if index != 0: 
@@ -88,99 +87,141 @@ def parseargs(args):
     if key: opts.append((key," ".join(values)))
     return opts
 
-def submit(prog,opts):
-    args = " ".join("-%s '%s'" % (key,value) for key,value in opts)
-    cmd = "python -m dumbo '%s' %s" % (prog,args)
-    print "Command:",cmd
-    return os.system(cmd)
-
-def stream(prog,opts):
-    def find_arg(key,description,default):
-        matches = map(itemgetter(1),filter(lambda x: x[0]==key,opts))
-        if matches: 
-            for match in matches: print "%s: %s" % (description,match)
-        else:
-            if default: 
-                opts.append((key,default))
-                print "%s: %s" % (description,default)
-            else:
-                input = None
-                while not input: input = raw_input("%s: " % description)
-                opts.append((key,input))
-    find_arg("hadoop","Hadoop home",None)
-    find_arg("input","Input path",None)
-    find_arg("output","Output path",None)
-    find_arg("name","Job name",prog)
-    find_arg("python","Python command","python")
-    find_arg("iteration","Iteration","0")
-    find_arg("delinputs","Delete inputs","no")
-
-    added_opts_keys = ["hadoop","name","python","iteration","delinputs",
-        "libjar","inputformat","numMapTasks"]
-    added_opts = dict((key,[]) for key in added_opts_keys)
+def delopts(opts,keys):
+    deleted = dict((key,[]) for key in keys)
     key,delindexes = None,[]
     for index,(key,value) in enumerate(opts):
-        if added_opts.has_key(key):
-            added_opts[key].append(value)
+        key = key.lower()
+        if deleted.has_key(key):
+            deleted[key].append(value)
             delindexes.append(index)
-    for delindex in reversed(delindexes): del opts[delindex] 
+    for delindex in reversed(delindexes): del opts[delindex]
+    return deleted
 
-    hadoop = added_opts["hadoop"][0]
-    streamingjardir = hadoop + "/contrib/streaming"
-    if not os.path.exists(streamingjardir):
-        streamingjardir = hadoop + "/contrib"
-    if not os.path.exists(streamingjardir):
-        streamingjardir = hadoop + "/build/contrib/streaming"
-    if not os.path.exists(streamingjardir): 
-        streamingjardir = hadoop + "/build/contrib"
-    dumbojardir = hadoop + "/contrib/dumbo"
-    if not os.path.exists(dumbojardir):
-        dumbojardir = hadoop + "/contrib"
-    if not os.path.exists(dumbojardir):
-        dumbojardir = hadoop + "/build/contrib/dumbo"
-    if not os.path.exists(dumbojardir): 
-        dumbojardir = hadoop + "/build/contrib"
+def execute(cmd,opts=[],precmd="",printcmd=True):
+    if precmd: cmd = " ".join((precmd,cmd))
+    args = " ".join("-%s '%s'" % (key,value) for key,value in opts)
+    if args: cmd = " ".join((cmd,args))
+    if printcmd: print "EXEC:",cmd
+    return os.system(cmd)
 
-    inputformat_shortcuts = {
-        "textascode": "TextAsCodeInputFormat", 
-        "sequencefileascode": "SequenceFileAsCodeInputFormat"}
-    if added_opts["inputformat"]:
-        inputformat = added_opts["inputformat"][0]
-        if inputformat_shortcuts.has_key(inputformat.lower()):
-            inputformat = "org.apache.hadoop.dumbo." + \
-                inputformat_shortcuts[inputformat.lower()]
-            regex = re.compile("hadoop.*dumbo\.jar")
-            added_opts["libjar"].append(dumbojardir + "/" + filter(regex.match,
-                os.listdir(dumbojardir))[-1])
-        opts.append(("inputformat",inputformat))
+def findjar(hadoop,name):
+    jardir = hadoop + "/contrib/" + name
+    if not os.path.exists(jardir): jardir = hadoop + "/contrib"
+    if not os.path.exists(jardir): jardir = hadoop + "/build/contrib/" + name
+    if not os.path.exists(jardir): jardir = hadoop + "/build/contrib"
+    regex = re.compile("hadoop.*" + name + "\.jar")
+    try: return jardir + "/" + filter(regex.match,os.listdir(jardir))[-1]
+    except: return None
 
-    python,iter = added_opts["python"][0],int(added_opts["iteration"][0])
+def envdef(varname,files,opts):
+    path=""
+    for file in files:
+        opts.append(("file",file))
+        path += file + ":"
+    return '%s="%s$%s"' % (varname,path,varname)
+
+def submit(prog,opts):
+    if execute("python -m dumbo run '%s'" % prog,opts,printcmd=False) == 32512:
+        print >>sys.stderr,'ERROR: Are you sure that "python" is on your path?'
+
+def stream(prog,opts):
+    addedopts = delopts(opts,["python","iteration","hadoop","fake"])
+    if not addedopts["python"]: python = "python"
+    else: python = addedopts["python"][0]
+    if not addedopts["iteration"]: iter = 0
+    else: iter = int(addedopts["iteration"][0])
     opts.append(("mapper","%s %s map %i" % (python,prog.split("/")[-1],iter)))
     opts.append(("reducer","%s %s red %i" % (python,prog.split("/")[-1],iter)))
+    if addedopts["fake"] and addedopts["fake"][0] == "yes":
+        os.system = lambda cmd: None  # not very clean, but it's easy and works
+    if not addedopts["hadoop"]: streamlocally(prog,opts)
+    else: streamonhadoop(prog,opts,addedopts["hadoop"][0])
+   
+def streamlocally(prog,opts):
+    addedopts = delopts(opts,["input","output","mapper","reducer","libpy",
+        "delinputs"])
+    mapper,reducer = addedopts["mapper"][0],addedopts["reducer"][0]
+    if (not addedopts["input"]) or (not addedopts["output"]):
+        print >>sys.stderr,"ERROR: input or output not specified"
+        sys.exit(1)
+    input,output = addedopts["input"][0],addedopts["output"][0]
+    pythonenv = envdef("PYTHONPATH",addedopts["libpy"],opts)
+    retval = execute("%s %s < '%s' | LC_ALL=C sort | %s %s > '%s'" % \
+        (pythonenv,mapper,input,pythonenv,reducer,output))
+    if addedopts["delinputs"] and addedopts["delinputs"][0] == "yes":
+        for file in addedopts["input"]: execute("rm " + file)
+    sys.exit(retval)
+
+def streamonhadoop(prog,opts,hadoop):
+    addedopts = delopts(opts,["name","delinputs","libpy","libjar","inputformat",
+        "nummaptasks","numreducetasks"])
     opts.append(("file",prog))
     opts.append(("file",sys.argv[0]))
-    opts.append(("jobconf","mapred.job.name=%s" % added_opts["name"][0]))
-    if added_opts["numMapTasks"]: opts.append(("jobconf",
-        "mapred.map.tasks=%s" % added_opts["numMapTasks"][0]))
-
-    classpath=""
-    for libjar in added_opts["libjar"]:
-        opts.append(("file",libjar))
-        classpath += libjar + ":"
-    regex = re.compile("hadoop.*streaming\.jar")
-    cmd = "%s/bin/hadoop jar %s/%s" % (hadoop,streamingjardir,
-        filter(regex.match,os.listdir(streamingjardir))[-1])
-    args = " ".join("-%s '%s'" % (key,value) for key,value in opts)
-    retval = os.system('HADOOP_CLASSPATH="%s$HADOOP_CLASSPATH" %s' % \
-        (classpath," ".join((cmd,args))))
-    if added_opts["delinputs"][0] == "yes":
+    if not addedopts["name"]: opts.append(("jobconf","mapred.job.name=" + prog))
+    else: opts.append(("jobconf","mapred.job.name=%s" % addedopts["name"][0]))
+    if addedopts["nummaptasks"]: opts.append(("jobconf",
+        "mapred.map.tasks=%s" % addedopts["nummaptasks"][0]))
+    if addedopts["numreducetasks"]: opts.append(("numReduceTasks",
+        addedopts["numreducetasks"][0]))
+    streamingjar,dumbojar = findjar(hadoop,"streaming"),findjar(hadoop,"dumbo")
+    if not streamingjar:
+        print >>sys.stderr,"ERROR: Streaming jar not found"
+        sys.exit(0)
+    if (not dumbojar) and addedopts["inputformat"]:
+        print >>sys.stderr,"ERROR: Dumbo jar not found"
+        sys.exit(0)
+    else:
+        inputformat_shortcuts = {
+            "textascode": "TextAsCodeInputFormat", 
+            "sequencefileascode": "SequenceFileAsCodeInputFormat"}
+        if addedopts["inputformat"]:
+            inputformat = addedopts["inputformat"][0]
+            if inputformat_shortcuts.has_key(inputformat.lower()):
+                inputformat = "org.apache.hadoop.dumbo." + \
+                    inputformat_shortcuts[inputformat.lower()]
+                addedopts["libjar"].append(dumbojar)
+            opts.append(("inputformat",inputformat))
+    pythonenv = envdef("PYTHONPATH",addedopts["libpy"],opts)
+    hadoopenv = envdef("HADOOP_CLASSPATH",addedopts["libjar"],opts) 
+    cmd = hadoop + "/bin/hadoop jar " + streamingjar
+    retval = execute(cmd,opts," ".join((pythonenv,hadoopenv)))
+    if addedopts["delinputs"] and addedopts["delinputs"][0] == "yes":
         for key,value in opts:
-            if key == "input": 
-                os.system("%s/bin/hadoop dfs -rmr %s" % (hadoop,value))
+            if key == "input":
+                execute("%s/bin/hadoop dfs -rmr %s" % (hadoop,value))
     sys.exit(retval)
+    
+def cat(path,opts):
+    addedopts = delopts(opts,["hadoop","type","libjar"])
+    if not addedopts["hadoop"]:
+        print >>sys.stderr,"ERROR: Hadoop dir not specified"
+        sys.exit(0)
+    hadoop = addedopts["hadoop"][0]
+    dumbojar = findjar(hadoop,"dumbo")
+    if not dumbojar:
+        print >>sys.stderr,"ERROR: Dumbo jar not found"
+        sys.exit(0)
+    if not addedopts["type"]: type = "text"
+    else: type = addedopts["type"][0]
+    hadoopenv = envdef("HADOOP_CLASSPATH",addedopts["libjar"],opts)
+    try:
+        if type[:4] == "text": codetype = "textascode"
+        else: codetype = "sequencefileascode"
+        process = os.popen("%s %s/bin/hadoop jar %s %s %s" % \
+            (hadoopenv,hadoop,dumbojar,codetype,path))    
+        if type[-6:] == "ascode": outputs = dumpcode(loadcode(process))
+        else: outputs = dumptext(loadcode(process))
+        for output in outputs: print "\t".join(output)
+        process.close()
+    except IOError: pass  # ignore
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print "Usage: python -m dumbo <python program> [<options>]"
+        print "Usages:"
+        print "  python -m dumbo run <python program> [<options>]"
+        print "  python -m dumbo cat <path> [<options>]"
         sys.exit(1)
-    stream(sys.argv[1],parseargs(sys.argv[1:]))
+    if sys.argv[1] == "run": stream(sys.argv[2],parseargs(sys.argv[2:]))
+    elif sys.argv[1] == "cat": cat(sys.argv[2],parseargs(sys.argv[2:]))
+    else: stream(sys.argv[1],parseargs(sys.argv[1:]))
