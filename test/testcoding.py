c48e8e6b3587cd85ed4912140ff41194a07f3e08
Klaas Bosteels <klbostee@T60p.(none)>
2008-09-28 15:32:25 +0200

    turned example testing code into a real unit test

--- test/testcoding.py
+++ test/testcoding.py
@@ -1,14 +1,26 @@
-import dumbo
+import dumbo,unittest
 
-def dotest(data):
-    print data
-    dumped = dumbo.dumpcode([[data]]).next()[0]
-    print dumped
-    print dumbo.loadcode([dumped]).next()[0]==data
-dotest((1,(2,(3,4)),5))
-dotest([(1,2),3,4])
-dotest(tuple())
-dotest([])
-dotest([[],1])
-dotest("{\"key\": 1}")
+class TestCoding(unittest.TestCase):
 
+    def dotest(self,data):
+        dumped = dumbo.dumpcode([[data]]).next()[0]
+        self.assertEqual(dumbo.loadcode([dumped]).next()[0],data)
+
+    def testtuple(self):
+        self.dotest(tuple())
+        self.dotest((1,(2,(3,4)),5))
+
+    def testlist(self):
+        self.dotest([])
+        self.dotest([(1,2),3,4])
+
+    def testmap(self):
+        self.dotest({})
+        self.dotest({'key': 'value'})
+
+    def teststring(self):
+        self.dotest("{'key': 1}")
+
+if __name__ == "__main__":
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestCoding)
+    unittest.TextTestRunner(verbosity=2).run(suite)
