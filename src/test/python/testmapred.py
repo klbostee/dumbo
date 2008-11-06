5ef48ab63fcde826e030d8cdf51a9d5e4ba63194
Klaas Bosteels <klbostee@WE02C022.(none)>
2008-11-06 17:55:42 +0100

    added mapreduce unit test

---
+++ src/test/python/testmapred.py
@@ -0,0 +1,16 @@
+import dumbo,unittest
+
+class TestMapRed(unittest.TestCase):
+    def testwordcount(self):
+        def mapper(key,value):
+            for word in value.split(): yield word,1
+        def reducer(key,values):
+            yield key,sum(values)
+        input = enumerate(['one two','two one two'])
+        output = dict(dumbo.itermapred(input,mapper,reducer))
+        self.assertEqual(output['one'],2)
+        self.assertEqual(output['two'],3)
+
+if __name__ == "__main__":
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestMapReduce)
+    unittest.TextTestRunner(verbosity=2).run(suite)
