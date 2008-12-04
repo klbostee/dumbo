8c34f7384e36666ee010d9c9c5bcb2e2fa255378
Klaas Bosteels <klbostee@WE02C022.(none)>
2008-12-04 12:12:32 +0100

    simplified wordcount example

--- examples/wordcount.py
+++ examples/wordcount.py
@@ -11,21 +11,12 @@ Counts how many times each non-excluded word occurs:
 6
 """
 
-excludes = None
-
-def loadexcludes():
-    global excludes
-    file = open("excludes.txt","r")
-    excludes = set(line.strip() for line in file)
-    file.close()
-
 def mapper(key,value):
-    for word in value.split(): 
-        if not (word in excludes): yield word,1
+    for word in value.split(): yield word,1
 
 def reducer(key,values):
     yield key,sum(values)
 
 if __name__ == "__main__":
     import dumbo
-    dumbo.run(mapper,reducer,reducer,mapconf=loadexcludes)
+    dumbo.run(mapper,reducer,reducer)
