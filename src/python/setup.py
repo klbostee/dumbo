e3c04912d2ed6bf931f2f0e1b6ea3391f411be32
Klaas Bosteels <klbostee@WE02C022.(none)>
2009-01-29 11:17:46 +0100

    version bump

--- src/python/setup.py
+++ src/python/setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='dumbo',
-      version='0.20.22',
+      version='0.20.23',
       py_modules=['dumbo'],
       author='Klaas Bosteels',
       author_email='klaas@last.fm',
