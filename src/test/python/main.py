216a00613b95f7247ce0ead38c099fbfeef5108c
Klaas Bosteels <klaas.bosteels@gmail.com>
2009-03-09 22:33:47 +0100

    removed java code

--- src/test/python/main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from unittest import TextTestRunner,TestLoader
-from glob import glob
-from os.path import splitext
-
-modules = [splitext(file)[0] for file in glob("*.py") if file != "main.py"]
-tests = TestLoader().loadTestsFromNames(modules)
-t = TextTestRunner(verbosity = 2)
-sys.exit(len(t.run(tests).errors))
