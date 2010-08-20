cabe51d67e78284463b8d800093264565ffdb74b
Klaas Bosteels <klaas.bosteels@gmail.com>
2010-08-20 18:30:44 +0100

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'dumbo',
-    version = '0.21.27',
+    version = '0.21.28',
     author = 'Klaas Bosteels',
     author_email = 'klaas@last.fm',
     license = 'Apache Software License (ASF)',
